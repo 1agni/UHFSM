b80c3724410f3265eeb8ff109311f2c65f77d4ae
Agni Ananda <agniananda@Agnis-MacBook-Air.local>
2025-07-17 11:28:01 -0700

    Add missing tenants folder and Tenants component

---
+++ src/pages/tenants/Tenants.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tenants: React.FC = () => {
+  return (
+    <div>
+      <h1>Tenants Management</h1>
+      <p>Tenant management functionality will be implemented here.</p>
+    </div>
+  );
+};
+
+export default Tenants;
